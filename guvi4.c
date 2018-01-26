1b0c4023503d8ce6e212686a978fd05337e52ff5
VigneshNarayananR <35727469+VigneshNarayananR@users.noreply.github.com>
2018-01-26 11:02:50 +0530

    Update and rename README.md to guvi4.c

---
+++ guvi4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char x;
+    printf("Enter a Alphabet: ");
+    scanf("%c",&x);
+
+    if( (x>='a' && x<='z') || (x>='A' && x<='Z'))
+        printf("%c is an alphabet.",x);
+    else
+        printf("%c is not an alphabet.",x);
+
+    return 0;
+}
