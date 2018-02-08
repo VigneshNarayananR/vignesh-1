271faaa0f1829c68e01b0e8f3136b14de0473016
VigneshNarayananR <35727469+VigneshNarayananR@users.noreply.github.com>
2018-02-08 16:33:35 +0530

    Update guvi13.c

--- guvi13.c
+++ guvi13.c
@@ -2,14 +2,14 @@
 void main()
 {
 int a,n;
-printf("enter a value");
+printf("Enter the value:");
 scanf("%d",&a);
 if(a%2==0)
 {
-printf("num is not prime");
+printf("The number is not prime:");
 }
 else
 {
-printf("num is prime");
+printf("The number is prime:");
 }
 }
